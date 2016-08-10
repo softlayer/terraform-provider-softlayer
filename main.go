6efecd0a65ac43882fa3810e94dc5f7eeb277ff3
Renier Morales <renierm@us.ibm.com>
2016-08-10 18:30:02 -0400

    Try specifying vcs suffix to get 'go get' working

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.ibm.com/emerging-tech/terraform-provider-softlayer/softlayer"
+	"github.ibm.com/emerging-tech/terraform-provider-softlayer.git/softlayer"
 )
 
 func main() {
