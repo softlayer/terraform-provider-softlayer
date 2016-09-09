323db9a419bd02106e63e03ad7bbbd909cbe5192
Minsik Lee <minsikl@gmail.com>
2016-09-08 22:48:37 -0400

    Fix wrong test scenario in lb_vpx_test

--- softlayer/resource_softlayer_lb_vpx_service_test.go
+++ softlayer/resource_softlayer_lb_vpx_service_test.go
@@ -15,11 +15,17 @@ func TestAccSoftLayerLbVpxService_Basic(t *testing.T) {
 				Config: testAccCheckSoftLayerLbVpxServiceConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
-						"softlayer_lb_vpx.testacc_service", "name", "test_load_balancer_service"),
+						"softlayer_lb_vpx_service.testacc_service1", "name", "test_load_balancer_service1"),
 					resource.TestCheckResourceAttr(
-						"softlayer_lb_vpx.testacc_service", "destination_port", "89"),
+						"softlayer_lb_vpx_service.testacc_service1", "destination_port", "89"),
 					resource.TestCheckResourceAttr(
-						"softlayer_lb_vpx.testacc_service", "weight", "55"),
+						"softlayer_lb_vpx_service.testacc_service1", "weight", "55"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_vpx_service.testacc_service2", "name", "test_load_balancer_service2"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_vpx_service.testacc_service2", "destination_port", "89"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_vpx_service.testacc_service2", "weight", "55"),
 				),
 			},
 		},
@@ -27,40 +33,37 @@ func TestAccSoftLayerLbVpxService_Basic(t *testing.T) {
 }
 
 var testAccCheckSoftLayerLbVpxServiceConfig_basic = `
-resource "softlayer_virtual_guest" "terraform-acceptance-test-1" {
-    name = "terraform-test"
-    domain = "bar.example.com"
+
+resource "softlayer_virtual_guest" "vm1" {
+    name = "vm1"
+    domain = "example.com"
     image = "DEBIAN_7_64"
-    region = "ams01"
+    datacenter = "wdc01"
     public_network_speed = 10
     hourly_billing = true
     private_network_only = false
     cpu = 1
     ram = 1024
-    disks = [25, 10, 20]
-    user_data = "{\"value\":\"newvalue\"}"
-    dedicated_acct_host_only = true
+    disks = [25]
     local_disk = false
 }
 
-resource "softlayer_virtual_guest" "terraform-acceptance-test-2" {
-    name = "terraform-test"
-    domain = "bar.example.com"
+resource "softlayer_virtual_guest" "vm2" {
+    name = "vm2"
+    domain = "example.com"
     image = "DEBIAN_7_64"
-    region = "ams01"
+    datacenter = "wdc01"
     public_network_speed = 10
     hourly_billing = true
-	private_network_only = false
+    private_network_only = false
     cpu = 1
     ram = 1024
-    disks = [25, 10, 20]
-    user_data = "{\"value\":\"newvalue\"}"
-    dedicated_acct_host_only = true
+    disks = [25]
     local_disk = false
 }
 
-resource "softlayer_lb_vpx" "testacc_foobar_vpx" {
-    datacenter = "DALLAS06"
+resource "softlayer_lb_vpx" "testacc_vpx" {
+    datacenter = "wdc01"
     speed = 10
     version = "10.1"
     plan = "Standard"
@@ -69,17 +72,27 @@ resource "softlayer_lb_vpx" "testacc_foobar_vpx" {
 
 resource "softlayer_lb_vpx_vip" "testacc_vip" {
     name = "test_load_balancer_vip"
-    nad_controller_id = "${softlayer_lb_vpx.testacc_foobar_vpx.id}"
+    nad_controller_id = "${softlayer_lb_vpx.testacc_vpx.id}"
     load_balancing_method = "lc"
-    source_port = 81
+    source_port = 80
     type = "HTTP"
-    virtual_ip_address = "${softlayer_virtual_guest.terraform-acceptance-test-1.ipv4_address}"
+    virtual_ip_address = "${softlayer_lb_vpx.testacc_vpx.vip_pool[0]}"
 }
 
-resource "softlayer_lb_vpx_service" "testacc_service" {
-  name = "test_load_balancer_service"
+resource "softlayer_lb_vpx_service" "testacc_service1" {
+  name = "test_load_balancer_service1"
   vip_id = "${softlayer_lb_vpx_vip.testacc_vip.id}"
-  destination_ip_address = "${softlayer_virtual_guest.terraform-acceptance-test-2.ipv4_address}"
+  destination_ip_address = "${softlayer_virtual_guest.vm1.ipv4_address}"
+  destination_port = 89
+  weight = 55
+  connection_limit = 5000
+  health_check = "HTTP"
+}
+
+resource "softlayer_lb_vpx_service" "testacc_service2" {
+  name = "test_load_balancer_service2"
+  vip_id = "${softlayer_lb_vpx_vip.testacc_vip.id}"
+  destination_ip_address = "${softlayer_virtual_guest.vm2.ipv4_address}"
   destination_port = 89
   weight = 55
   connection_limit = 5000
