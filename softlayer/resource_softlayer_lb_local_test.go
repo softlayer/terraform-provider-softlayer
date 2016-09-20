c614b63610880ac2f53eefea8fba76c513bd62f4
Srinivas Dandibhotla <srinivad@us.ibm.com>
2016-09-20 03:28:35 -0500

    Added test function for dedicated local lb

--- softlayer/resource_softlayer_lb_local_test.go
+++ softlayer/resource_softlayer_lb_local_test.go
@@ -25,9 +25,39 @@ func TestAccSoftLayerLbLocal_Basic(t *testing.T) {
 	})
 }
 
+func TestAccSoftLayerLbLocalDedicated_Basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckSoftLayerLbLocalDedicatedConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "connections", "15000"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "datacenter", "tok02"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "ha_enabled", "false"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "dedicated", "true"),
+				),
+			},
+		},
+	})
+}
+
 const testAccCheckSoftLayerLbLocalConfig_basic = `
 resource "softlayer_lb_local" "testacc_foobar_lb" {
     connections = 15000
     datacenter    = "tok02"
     ha_enabled  = false
+}`
+
+const testAccCheckSoftLayerLbLocalDedicatedConfig_basic = `
+resource "softlayer_lb_local" "testacc_foobar_lb" {
+    connections = 15000
+    datacenter    = "tok02"
+    ha_enabled  = false
+    dedicated = true	
 }`
