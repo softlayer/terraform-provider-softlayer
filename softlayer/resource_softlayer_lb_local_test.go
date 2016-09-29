d56baf911638a910ca5616c1b7a61ba5c88795a0
riethm <riethm@users.noreply.github.com>
2016-09-29 18:11:57 -0400

    Feature/shared local lb #6
    * Support shared local load balancer
    
    * Add test function for dedicated local lb
    
    * Updated with documentation for dedicated and ssl_enabled properties
    
    * Added Default value for dedicated property
    
    * Add validation for ha_status
    
    * Enable certificate to be removed from local LB

--- softlayer/resource_softlayer_lb_local_test.go
+++ softlayer/resource_softlayer_lb_local_test.go
@@ -5,13 +5,37 @@ import (
 	"testing"
 )
 
-func TestAccSoftLayerLbLocal_Basic(t *testing.T) {
+func TestAccSoftLayerLbLocalShared_Basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
-				Config: testAccCheckSoftLayerLbLocalConfig_basic,
+				Config: testAccCheckSoftLayerLbLocalConfigShared_basic,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "connections", "250"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "datacenter", "tok02"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "ha_enabled", "false"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "dedicated", "false"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "ssl_enabled", "false"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccSoftLayerLbLocalDedicated_Basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckSoftLayerLbLocalDedicatedConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"softlayer_lb_local.testacc_foobar_lb", "connections", "15000"),
@@ -19,15 +43,27 @@ func TestAccSoftLayerLbLocal_Basic(t *testing.T) {
 						"softlayer_lb_local.testacc_foobar_lb", "datacenter", "tok02"),
 					resource.TestCheckResourceAttr(
 						"softlayer_lb_local.testacc_foobar_lb", "ha_enabled", "false"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "dedicated", "true"),
+					resource.TestCheckResourceAttr(
+						"softlayer_lb_local.testacc_foobar_lb", "ssl_enabled", "true"),
 				),
 			},
 		},
 	})
 }
 
-const testAccCheckSoftLayerLbLocalConfig_basic = `
+const testAccCheckSoftLayerLbLocalConfigShared_basic = `
+resource "softlayer_lb_local" "testacc_foobar_lb" {
+    connections = 250
+    datacenter    = "tok02"
+    ha_enabled  = false
+}`
+
+const testAccCheckSoftLayerLbLocalDedicatedConfig_basic = `
 resource "softlayer_lb_local" "testacc_foobar_lb" {
     connections = 15000
     datacenter    = "tok02"
     ha_enabled  = false
+    dedicated = true	
 }`
