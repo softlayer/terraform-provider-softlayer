c7296659666ad227353685442ac98cb58d554163
Srinivas Dandibhotla <srinivad@us.ibm.com>
2016-10-06 02:00:36 -0500

    Committing formatted global ip resource implementation code

--- softlayer/resource_softlayer_global_ip.go
+++ softlayer/resource_softlayer_global_ip.go
@@ -1,250 +1,250 @@
 package softlayer
 
 import (
-        "fmt"
-        "github.com/hashicorp/terraform/helper/resource"
-        "github.com/hashicorp/terraform/helper/schema"
-        "github.com/softlayer/softlayer-go/datatypes"
-        "github.com/softlayer/softlayer-go/filter"
-        "github.com/softlayer/softlayer-go/helpers/product"
-        "github.com/softlayer/softlayer-go/services"
-        "github.com/softlayer/softlayer-go/session"
-        "github.com/softlayer/softlayer-go/sl"
-        "log"
-        "strconv"
-        "time"
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/softlayer/softlayer-go/datatypes"
+	"github.com/softlayer/softlayer-go/filter"
+	"github.com/softlayer/softlayer-go/helpers/product"
+	"github.com/softlayer/softlayer-go/services"
+	"github.com/softlayer/softlayer-go/session"
+	"github.com/softlayer/softlayer-go/sl"
+	"log"
+	"strconv"
+	"time"
 )
 
 const (
-        AdditionalServicesPackageType                  = "ADDITIONAL_SERVICES"
-        AdditionalServicesGlobalIpAddressesPackageType = "ADDITIONAL_SERVICES_GLOBAL_IP_ADDRESSES"
+	AdditionalServicesPackageType                  = "ADDITIONAL_SERVICES"
+	AdditionalServicesGlobalIpAddressesPackageType = "ADDITIONAL_SERVICES_GLOBAL_IP_ADDRESSES"
 
-        GlobalIpMask = "id,ipAddress[ipAddress],destinationIpAddress[ipAddress]"
+	GlobalIpMask = "id,ipAddress[ipAddress],destinationIpAddress[ipAddress]"
 )
 
 func resourceSoftLayerGlobalIp() *schema.Resource {
-        return &schema.Resource{
-                Create:   resourceSoftLayerGlobalIpCreate,
-                Read:     resourceSoftLayerGlobalIpRead,
-                Update:   resourceSoftLayerGlobalIpUpdate,
-                Delete:   resourceSoftLayerGlobalIpDelete,
-                Exists:   resourceSoftLayerGlobalIpExists,
-                Importer: &schema.ResourceImporter{},
+	return &schema.Resource{
+		Create:   resourceSoftLayerGlobalIpCreate,
+		Read:     resourceSoftLayerGlobalIpRead,
+		Update:   resourceSoftLayerGlobalIpUpdate,
+		Delete:   resourceSoftLayerGlobalIpDelete,
+		Exists:   resourceSoftLayerGlobalIpExists,
+		Importer: &schema.ResourceImporter{},
 
-                Schema: map[string]*schema.Schema{
-                        "id": &schema.Schema{
-                                Type:     schema.TypeInt,
-                                Computed: true,
-                        },
+		Schema: map[string]*schema.Schema{
+			"id": &schema.Schema{
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
 
-                        "ip_address": &schema.Schema{
-                                Type:     schema.TypeString,
-                                Computed: true,
-                        },
+			"ip_address": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 
-                        "routes_to": &schema.Schema{
-                                Type:     schema.TypeString,
-                                Required: true,
-                        },
-                },
-        }
+			"routes_to": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
 }
 
 func resourceSoftLayerGlobalIpCreate(d *schema.ResourceData, meta interface{}) error {
-        sess := meta.(*session.Session)
-        
-        // Find price items with AdditionalServicesGlobalIpAddresses
-        productOrderContainer, err := buildGlobalIpProductOrderContainer(d, sess, AdditionalServicesGlobalIpAddressesPackageType)
-        if err != nil {
-                // Find price items with AdditionalServices
-                productOrderContainer, err = buildGlobalIpProductOrderContainer(d, sess, AdditionalServicesPackageType)
-                if err != nil {
-                        return fmt.Errorf("Error creating global ip: %s", err)
-                }
-        }
+	sess := meta.(*session.Session)
 
-        log.Println("[INFO] Creating global ip")
+	// Find price items with AdditionalServicesGlobalIpAddresses
+	productOrderContainer, err := buildGlobalIpProductOrderContainer(d, sess, AdditionalServicesGlobalIpAddressesPackageType)
+	if err != nil {
+		// Find price items with AdditionalServices
+		productOrderContainer, err = buildGlobalIpProductOrderContainer(d, sess, AdditionalServicesPackageType)
+		if err != nil {
+			return fmt.Errorf("Error creating global ip: %s", err)
+		}
+	}
 
-        receipt, err := services.GetProductOrderService(sess).
-                PlaceOrder(productOrderContainer, sl.Bool(false))
-        if err != nil {
-                return fmt.Errorf("Error during creation of global ip: %s", err)
-        }
+	log.Println("[INFO] Creating global ip")
 
-        globalIp, err := findGlobalIpByOrderId(sess, *receipt.OrderId)
-        d.SetId(fmt.Sprintf("%d", *globalIp.Id))
-        return resourceSoftLayerGlobalIpUpdate(d, meta)
+	receipt, err := services.GetProductOrderService(sess).
+		PlaceOrder(productOrderContainer, sl.Bool(false))
+	if err != nil {
+		return fmt.Errorf("Error during creation of global ip: %s", err)
+	}
+
+	globalIp, err := findGlobalIpByOrderId(sess, *receipt.OrderId)
+	d.SetId(fmt.Sprintf("%d", *globalIp.Id))
+	return resourceSoftLayerGlobalIpUpdate(d, meta)
 
 }
 
 func resourceSoftLayerGlobalIpRead(d *schema.ResourceData, meta interface{}) error {
-        sess := meta.(*session.Session)
-        service := services.GetNetworkSubnetIpAddressGlobalService(sess)
+	sess := meta.(*session.Session)
+	service := services.GetNetworkSubnetIpAddressGlobalService(sess)
 
-        globalIpId, err := strconv.Atoi(d.Id())
-        if err != nil {
-                return fmt.Errorf("Not a valid global ip ID, must be an integer: %s", err)
-        }
+	globalIpId, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return fmt.Errorf("Not a valid global ip ID, must be an integer: %s", err)
+	}
 
-        globalIp, err := service.Id(globalIpId).Mask(GlobalIpMask).GetObject()
-        if err != nil {
-                if apiErr, ok := err.(sl.Error); ok && apiErr.StatusCode == 404 {
-                        d.SetId("")
-                        return nil
-                }
+	globalIp, err := service.Id(globalIpId).Mask(GlobalIpMask).GetObject()
+	if err != nil {
+		if apiErr, ok := err.(sl.Error); ok && apiErr.StatusCode == 404 {
+			d.SetId("")
+			return nil
+		}
 
-                return fmt.Errorf("Error retrieving Global Ip: %s", err)
-        }
+		return fmt.Errorf("Error retrieving Global Ip: %s", err)
+	}
 
-        d.Set("id", *globalIp.Id)
-        d.Set("ip_address", *globalIp.IpAddress.IpAddress)
-        d.Set("routes_to", *globalIp.DestinationIpAddress.IpAddress)        
+	d.Set("id", *globalIp.Id)
+	d.Set("ip_address", *globalIp.IpAddress.IpAddress)
+	d.Set("routes_to", *globalIp.DestinationIpAddress.IpAddress)
 
-        return nil
+	return nil
 }
 
 func resourceSoftLayerGlobalIpUpdate(d *schema.ResourceData, meta interface{}) error {
-        sess := meta.(*session.Session)
-        service := services.GetNetworkSubnetIpAddressGlobalService(sess)
+	sess := meta.(*session.Session)
+	service := services.GetNetworkSubnetIpAddressGlobalService(sess)
 
-        globalIpId, err := strconv.Atoi(d.Id())
-        if err != nil {
-                return fmt.Errorf("Not a valid global ip ID, must be an integer: %s", err)
-        }
-        
-        if d.HasChange("routes_to") {
-                _, err = service.Id(globalIpId).Route(sl.String(d.Get("routes_to").(string)))
-                if err != nil {
-                        return fmt.Errorf("Error editing Global Ip: %s", err)
-                }
-        }        
+	globalIpId, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return fmt.Errorf("Not a valid global ip ID, must be an integer: %s", err)
+	}
 
-        return resourceSoftLayerGlobalIpRead(d, meta)
+	if d.HasChange("routes_to") {
+		_, err = service.Id(globalIpId).Route(sl.String(d.Get("routes_to").(string)))
+		if err != nil {
+			return fmt.Errorf("Error editing Global Ip: %s", err)
+		}
+	}
+
+	return resourceSoftLayerGlobalIpRead(d, meta)
 }
 
 func resourceSoftLayerGlobalIpDelete(d *schema.ResourceData, meta interface{}) error {
-        sess := meta.(*session.Session)
-        service := services.GetNetworkSubnetIpAddressGlobalService(sess)
+	sess := meta.(*session.Session)
+	service := services.GetNetworkSubnetIpAddressGlobalService(sess)
 
-        globalIpId, err := strconv.Atoi(d.Id())
-        if err != nil {
-                return fmt.Errorf("Not a valid global ip ID, must be an integer: %s", err)
-        }
+	globalIpId, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return fmt.Errorf("Not a valid global ip ID, must be an integer: %s", err)
+	}
 
-        billingItem, err := service.Id(globalIpId).GetBillingItem()
-        if err != nil {
-                return fmt.Errorf("Error deleting global ip: %s", err)
-        }
+	billingItem, err := service.Id(globalIpId).GetBillingItem()
+	if err != nil {
+		return fmt.Errorf("Error deleting global ip: %s", err)
+	}
 
-        if billingItem.Id == nil {
-                return nil
-        }
+	if billingItem.Id == nil {
+		return nil
+	}
 
-        _, err = services.GetBillingItemService(sess).Id(*billingItem.Id).CancelService()
+	_, err = services.GetBillingItemService(sess).Id(*billingItem.Id).CancelService()
 
-        return err
+	return err
 }
 
 func resourceSoftLayerGlobalIpExists(d *schema.ResourceData, meta interface{}) (bool, error) {
-        sess := meta.(*session.Session)
-        service := services.GetNetworkSubnetIpAddressGlobalService(sess)
+	sess := meta.(*session.Session)
+	service := services.GetNetworkSubnetIpAddressGlobalService(sess)
 
-        globalIpId, err := strconv.Atoi(d.Id())
-        if err != nil {
-                return false, fmt.Errorf("Not a valid ID, must be an integer: %s", err)
-        }
+	globalIpId, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return false, fmt.Errorf("Not a valid ID, must be an integer: %s", err)
+	}
 
-        result, err := service.Id(globalIpId).GetObject()
-        if err != nil {
-                return false, fmt.Errorf("Error retrieving global ip: %s", err)
-        }
-        return result.Id != nil && *result.Id == globalIpId, nil
+	result, err := service.Id(globalIpId).GetObject()
+	if err != nil {
+		return false, fmt.Errorf("Error retrieving global ip: %s", err)
+	}
+	return result.Id != nil && *result.Id == globalIpId, nil
 }
 
 func findGlobalIpByOrderId(sess *session.Session, orderId int) (datatypes.Network_Subnet_IpAddress_Global, error) {
-        stateConf := &resource.StateChangeConf{
-                Pending: []string{"pending"},
-                Target:  []string{"complete"},
-                Refresh: func() (interface{}, string, error) {
-                        globalIps, err := services.GetAccountService(sess).
-                                Filter(filter.Path("globalIpRecords.billingItem.orderItem.order.id").
-                                        Eq(strconv.Itoa(orderId)).Build()).
-                                Mask("id").
-                                GetGlobalIpRecords()
-                        if err != nil {
-                                return datatypes.Network_Subnet_IpAddress_Global{}, "", err
-                        }
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"pending"},
+		Target:  []string{"complete"},
+		Refresh: func() (interface{}, string, error) {
+			globalIps, err := services.GetAccountService(sess).
+				Filter(filter.Path("globalIpRecords.billingItem.orderItem.order.id").
+					Eq(strconv.Itoa(orderId)).Build()).
+				Mask("id").
+				GetGlobalIpRecords()
+			if err != nil {
+				return datatypes.Network_Subnet_IpAddress_Global{}, "", err
+			}
 
-                        if len(globalIps) == 1 {
-                                return globalIps[0], "complete", nil
-                        } else if len(globalIps) == 0 {
-                                return nil, "pending", nil
-                        } else {
-                                return nil, "", fmt.Errorf("Expected one global ip: %s", err)
-                        }
-                },
-                Timeout:    10 * time.Minute,
-                Delay:      5 * time.Second,
-                MinTimeout: 3 * time.Second,
-        }
+			if len(globalIps) == 1 {
+				return globalIps[0], "complete", nil
+			} else if len(globalIps) == 0 {
+				return nil, "pending", nil
+			} else {
+				return nil, "", fmt.Errorf("Expected one global ip: %s", err)
+			}
+		},
+		Timeout:    10 * time.Minute,
+		Delay:      5 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
 
-        pendingResult, err := stateConf.WaitForState()
+	pendingResult, err := stateConf.WaitForState()
 
-        if err != nil {
-                return datatypes.Network_Subnet_IpAddress_Global{}, err
-        }
+	if err != nil {
+		return datatypes.Network_Subnet_IpAddress_Global{}, err
+	}
 
-        if result, ok := pendingResult.(datatypes.Network_Subnet_IpAddress_Global); ok {
-                return result, nil
-        }        
+	if result, ok := pendingResult.(datatypes.Network_Subnet_IpAddress_Global); ok {
+		return result, nil
+	}
 
-        return datatypes.Network_Subnet_IpAddress_Global{},
-                fmt.Errorf("Cannot find global ip with order id '%d'", orderId)
+	return datatypes.Network_Subnet_IpAddress_Global{},
+		fmt.Errorf("Cannot find global ip with order id '%d'", orderId)
 }
 
 func buildGlobalIpProductOrderContainer(d *schema.ResourceData, sess *session.Session, packageType string) (
-        *datatypes.Container_Product_Order_Network_Subnet, error) {    
-   
-        // 1. Get a package
-        pkg, err := product.GetPackageByType(sess, packageType)
-        if err != nil {
-                return &datatypes.Container_Product_Order_Network_Subnet{}, err
-        }
+	*datatypes.Container_Product_Order_Network_Subnet, error) {
 
-        // 2. Get all prices for the package
-        productItems, err := product.GetPackageProducts(sess, *pkg.Id)
-        if err != nil {
-                return &datatypes.Container_Product_Order_Network_Subnet{}, err
-        }
+	// 1. Get a package
+	pkg, err := product.GetPackageByType(sess, packageType)
+	if err != nil {
+		return &datatypes.Container_Product_Order_Network_Subnet{}, err
+	}
 
-        // 3. Find global ip prices
-        // the following looks for only IPV4 Global Ips only
-        globalIpKeyname := "GLOBAL_IPV4"
+	// 2. Get all prices for the package
+	productItems, err := product.GetPackageProducts(sess, *pkg.Id)
+	if err != nil {
+		return &datatypes.Container_Product_Order_Network_Subnet{}, err
+	}
 
-        // 4. Select items with a matching keyname
-        globalIpItems := []datatypes.Product_Item{}
-        for _, item := range productItems {
-                if *item.KeyName == globalIpKeyname {
-                        globalIpItems = append(globalIpItems, item)
-                }
-        }
-        
-        if len(globalIpItems) == 0 {
-                return &datatypes.Container_Product_Order_Network_Subnet{},
-                        fmt.Errorf("No product items matching %s could be found", globalIpKeyname)
-        }
-        
-        productOrderContainer := datatypes.Container_Product_Order_Network_Subnet{
-                Container_Product_Order: datatypes.Container_Product_Order{
-                        PackageId: pkg.Id,
-                        Prices: []datatypes.Product_Item_Price{
-                                {
-                                        Id: globalIpItems[0].Prices[0].Id,
-                                },
-                        },
-                        Quantity: sl.Int(1),
-                },
-        }
+	// 3. Find global ip prices
+	// the following looks for only IPV4 Global Ips only
+	globalIpKeyname := "GLOBAL_IPV4"
 
-        return &productOrderContainer, nil
+	// 4. Select items with a matching keyname
+	globalIpItems := []datatypes.Product_Item{}
+	for _, item := range productItems {
+		if *item.KeyName == globalIpKeyname {
+			globalIpItems = append(globalIpItems, item)
+		}
+	}
+
+	if len(globalIpItems) == 0 {
+		return &datatypes.Container_Product_Order_Network_Subnet{},
+			fmt.Errorf("No product items matching %s could be found", globalIpKeyname)
+	}
+
+	productOrderContainer := datatypes.Container_Product_Order_Network_Subnet{
+		Container_Product_Order: datatypes.Container_Product_Order{
+			PackageId: pkg.Id,
+			Prices: []datatypes.Product_Item_Price{
+				{
+					Id: globalIpItems[0].Prices[0].Id,
+				},
+			},
+			Quantity: sl.Int(1),
+		},
+	}
+
+	return &productOrderContainer, nil
 }
--- softlayer/resource_softlayer_global_ip_test.go
+++ softlayer/resource_softlayer_global_ip_test.go
@@ -1,108 +1,108 @@
 package softlayer
 
 import (
-        "fmt"
-        "strconv"
-        "testing"
+	"fmt"
+	"strconv"
+	"testing"
 
-        "github.com/hashicorp/terraform/helper/resource"
-        "github.com/hashicorp/terraform/terraform"
-        "github.com/softlayer/softlayer-go/datatypes"
-        "github.com/softlayer/softlayer-go/services"
-        "github.com/softlayer/softlayer-go/session"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/softlayer/softlayer-go/datatypes"
+	"github.com/softlayer/softlayer-go/services"
+	"github.com/softlayer/softlayer-go/session"
 )
 
 func TestAccSoftLayerGlobalIp_Basic(t *testing.T) {
-        var globalIp datatypes.Network_Subnet_IpAddress_Global
+	var globalIp datatypes.Network_Subnet_IpAddress_Global
 
-        resource.Test(t, resource.TestCase{
-                PreCheck:     func() { testAccPreCheck(t) },
-                Providers:    testAccProviders,
-                CheckDestroy: testAccCheckSoftLayerGlobalIpDestroy,
-                Steps: []resource.TestStep{
-                        resource.TestStep{
-                                Config: testAccCheckSoftLayerGlobalIpConfig_basic,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testAccCheckSoftLayerGlobalIpExists("softlayer_global_ip.test-global-ip", &globalIp),
-                                        testAccCheckSoftLayerGlobalIpAttributes(&globalIp),
-                                        resource.TestCheckResourceAttr(
-                                                "softlayer_global_ip.test-global-ip", "routes_to", "119.81.82.163"),
-                                ),
-                        }, 
-                        
-                        resource.TestStep{
-                                Config: testAccCheckSoftLayerGlobalIpConfig_updated,
-                                Check: resource.ComposeTestCheckFunc(
-                                        testAccCheckSoftLayerGlobalIpExists("softlayer_global_ip.updated-test-global-ip", &globalIp),
-                                        resource.TestCheckResourceAttr(
-                                                "softlayer_global_ip.updated-test-global-ip", "routes_to", "119.81.82.160"),
-                                ),
-                        },
-                },
-        })
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSoftLayerGlobalIpDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckSoftLayerGlobalIpConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSoftLayerGlobalIpExists("softlayer_global_ip.test-global-ip", &globalIp),
+					testAccCheckSoftLayerGlobalIpAttributes(&globalIp),
+					resource.TestCheckResourceAttr(
+						"softlayer_global_ip.test-global-ip", "routes_to", "119.81.82.163"),
+				),
+			},
+
+			resource.TestStep{
+				Config: testAccCheckSoftLayerGlobalIpConfig_updated,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSoftLayerGlobalIpExists("softlayer_global_ip.updated-test-global-ip", &globalIp),
+					resource.TestCheckResourceAttr(
+						"softlayer_global_ip.updated-test-global-ip", "routes_to", "119.81.82.160"),
+				),
+			},
+		},
+	})
 }
 
 func testAccCheckSoftLayerGlobalIpDestroy(s *terraform.State) error {
-        service := services.GetNetworkSubnetIpAddressGlobalService(testAccProvider.Meta().(*session.Session))
+	service := services.GetNetworkSubnetIpAddressGlobalService(testAccProvider.Meta().(*session.Session))
 
-        for _, rs := range s.RootModule().Resources {
-                if rs.Type != "softlayer_global_ip" {
-                        continue
-                }
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "softlayer_global_ip" {
+			continue
+		}
 
-                globalIpId, _ := strconv.Atoi(rs.Primary.ID)
+		globalIpId, _ := strconv.Atoi(rs.Primary.ID)
 
-                // Try to find the global ip
-                _, err := service.Id(globalIpId).GetObject()
+		// Try to find the global ip
+		_, err := service.Id(globalIpId).GetObject()
 
-                if err == nil {
-                        return fmt.Errorf("Global Ip still exists")
-                }
-        }
+		if err == nil {
+			return fmt.Errorf("Global Ip still exists")
+		}
+	}
 
-        return nil
+	return nil
 }
 
 func testAccCheckSoftLayerGlobalIpAttributes(globalIp *datatypes.Network_Subnet_IpAddress_Global) resource.TestCheckFunc {
-        return func(s *terraform.State) error {
+	return func(s *terraform.State) error {
 
-                if *globalIp.DestinationIpAddress.IpAddress != "119.81.82.160" {
-                        return fmt.Errorf("Bad destination ip address: %s", *globalIp.DestinationIpAddress.IpAddress)
-                }
+		if *globalIp.DestinationIpAddress.IpAddress != "119.81.82.160" {
+			return fmt.Errorf("Bad destination ip address: %s", *globalIp.DestinationIpAddress.IpAddress)
+		}
 
-                return nil
-        }
+		return nil
+	}
 }
 
 func testAccCheckSoftLayerGlobalIpExists(n string, globalIp *datatypes.Network_Subnet_IpAddress_Global) resource.TestCheckFunc {
-        return func(s *terraform.State) error {
-                rs, ok := s.RootModule().Resources[n]
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
 
-                if !ok {
-                        return fmt.Errorf("Not found: %s", n)
-                }
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
 
-                if rs.Primary.ID == "" {
-                        return fmt.Errorf("No Record ID is set")
-                }
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
 
-                globalIpId, _ := strconv.Atoi(rs.Primary.ID)
+		globalIpId, _ := strconv.Atoi(rs.Primary.ID)
 
-                service := services.GetNetworkSubnetIpAddressGlobalService(testAccProvider.Meta().(*session.Session))
-                foundGlobalIp, err := service.Id(globalIpId).GetObject()
+		service := services.GetNetworkSubnetIpAddressGlobalService(testAccProvider.Meta().(*session.Session))
+		foundGlobalIp, err := service.Id(globalIpId).GetObject()
 
-                if err != nil {
-                        return err
-                }
+		if err != nil {
+			return err
+		}
 
-                if strconv.Itoa(int(*foundGlobalIp.Id)) != rs.Primary.ID {
-                        return fmt.Errorf("Record not found")
-                }
+		if strconv.Itoa(int(*foundGlobalIp.Id)) != rs.Primary.ID {
+			return fmt.Errorf("Record not found")
+		}
 
-                *globalIp = foundGlobalIp
+		*globalIp = foundGlobalIp
 
-                return nil
-        }
+		return nil
+	}
 }
 
 const testAccCheckSoftLayerGlobalIpConfig_basic = `
