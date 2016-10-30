ba7809d3d18a0cc3228013d1a7d02ebfaee2e96a
Renier Morales <renierm@us.ibm.com>
2016-10-30 01:40:12 -0400

    Implicitly import ssh key upon resource create if key already exists
    Before creating the SSH key, we first check whether it exists in
    SoftLayer. We do this by calculating the fingerprint of the key
    locally. Then we look for keys matching that fingerprint (using a
    filter). If a matching key is found, we import its details (e.g. id).
    We still update the label and notes as specified in the config if
    pertinent.

--- softlayer/resource_softlayer_ssh_key.go
+++ softlayer/resource_softlayer_ssh_key.go
@@ -1,6 +1,9 @@
 package softlayer
 
 import (
+	"crypto/md5"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"log"
 	"strconv"
@@ -8,6 +11,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/softlayer/softlayer-go/datatypes"
+	"github.com/softlayer/softlayer-go/filter"
 	"github.com/softlayer/softlayer-go/services"
 	"github.com/softlayer/softlayer-go/session"
 	"github.com/softlayer/softlayer-go/sl"
@@ -59,6 +63,59 @@ func resourceSoftLayerSSHKey() *schema.Resource {
 func resourceSoftLayerSSHKeyCreate(d *schema.ResourceData, meta interface{}) error {
 	sess := meta.(*session.Session)
 	service := services.GetSecuritySshKeyService(sess)
+
+	// First check if the key exits by fingerprint
+	// If so, set the Id (and fingerprint), but update notes and label (if any)
+	key := d.Get("public_key").(string)
+	parts := strings.Fields(key)
+	if len(parts) < 2 {
+		return errors.New("Invalid public key specified")
+	}
+
+	k, err := base64.StdEncoding.DecodeString(parts[1])
+	if err != nil {
+		return fmt.Errorf("Error decoding the public key: %s", err)
+	}
+
+	fp := md5.Sum([]byte(k))
+	prints := make([]string, len(fp))
+	for i, b := range fp {
+		prints[i] = fmt.Sprintf("%02x", b)
+	}
+	fingerprint := strings.Join(prints, ":")
+
+	keys, err := services.GetAccountService(sess).
+		Filter(filter.Path("sshKeys.fingerprint").Eq(fingerprint).Build()).
+		GetSshKeys()
+	if err == nil && len(keys) > 0 {
+		slKey := keys[0]
+		id := *slKey.Id
+		slKey.Id = nil
+		d.SetId(fmt.Sprintf("%d", id))
+		d.Set("fingerprint", fingerprint)
+		editKey := false
+
+		notes := d.Get("notes").(string)
+		if notes != "" && (slKey.Notes == nil || notes != *slKey.Notes) {
+			slKey.Notes = sl.String(notes)
+			editKey = true
+		} else if slKey.Notes != nil {
+			d.Set("notes", *slKey.Notes)
+		}
+
+		label := d.Get("label").(string)
+		if label != *slKey.Label {
+			slKey.Label = sl.String(d.Get("label").(string))
+			editKey = true
+		}
+
+		if editKey {
+			_, err = service.Id(id).EditObject(&slKey)
+			return err
+		}
+
+		return nil
+	} // End of "Import"
 
 	// Build up our creation options
 	opts := datatypes.Security_Ssh_Key{
