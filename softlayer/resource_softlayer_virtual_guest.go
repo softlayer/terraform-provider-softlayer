70cfeff25bdb2be8374b3c480d5e6e635b580526
Renier Morales <renierm@us.ibm.com>
2016-11-10 13:46:22 -0400

    Ensure two consecutive checks of no active transactions

--- softlayer/resource_softlayer_virtual_guest.go
+++ softlayer/resource_softlayer_virtual_guest.go
@@ -22,6 +22,8 @@ import (
 	"github.com/softlayer/softlayer-go/sl"
 )
 
+const assurityLevel = 1
+
 func genId() (interface{}, error) {
 	numBytes := 8
 	bytes := make([]byte, numBytes)
@@ -809,6 +811,7 @@ func WaitForNoActiveTransactions(d *schema.ResourceData, meta interface{}) (inte
 		return nil, fmt.Errorf("The instance ID %s must be numeric", d.Id())
 	}
 
+	assurity := assurityLevel
 	stateConf := &resource.StateChangeConf{
 		Pending: []string{"retry", "active"},
 		Target:  []string{"idle"},
@@ -824,7 +827,12 @@ func WaitForNoActiveTransactions(d *schema.ResourceData, meta interface{}) (inte
 			}
 
 			if len(transactions) == 0 {
-				return transactions, "idle", nil
+				if assurity == 0 {
+					return transactions, "idle", nil
+				}
+				assurity--
+			} else {
+				assurity = assurityLevel
 			}
 
 			return transactions, "active", nil
